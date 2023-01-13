4e9772b7922a23a6b04293a20563a30922edb09b
karthik1900 <40236661+karthik1900@users.noreply.github.com>
2023-01-13 16:21:10 +0000

    renaming

--- Serving/processing.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-async def pre_process(inputs):
-    processed_inputs = pd.DataFrame(inputs, index = [0])
-    return processed_inputs
-
-def post_process(predictions, model_name):
-    return {"progression" : predictions[0], "model": model_name}
