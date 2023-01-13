bc15f801209ece5caf855f72da5de43d5000648b
karthik1900 <40236661+karthik1900@users.noreply.github.com>
2023-01-13 21:40:20 +0530

    Update models.py

--- Serving/models.py
+++ Serving/models.py
@@ -1,16 +1,15 @@
 from joblib import load
 async def linear_reg_model(data):
-    model = load("./models/linear_reg.joblib")
-    predictions = model.predict(X)
+    model = load("/model-serve/models/linear_reg.joblib")
+    predictions = model.predict(data)
     return predictions
 
 async def lasso_model(data):
-    model = load("./models/lasso.joblib")
-    predictions = model.predict(X)
+    model = load("/model-serve/models/lasso.joblib")
+    predictions = model.predict(data)
     return predictions
 
 async def dtree_model(data):
-    model = load("./models/dtree.joblib")
-    X = pd.DataFrame(test_case, index = [0])
-    predictions = model.predict(X)
+    model = load("/model-serve/models/dtree.joblib")
+    predictions = model.predict(data)
     return predictions
