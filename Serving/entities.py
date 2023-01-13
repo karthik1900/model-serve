f4778f281ca68e3448ea89e88eb44f9bb3fdcd03
unknown <raokarthik19@gmail.com>
2023-01-13 18:55:53 +0530

    serve application

--- Serving/entities.py
+++ Serving/entities.py
@@ -2,32 +2,18 @@ from pydantic import BaseModel, validator
 from typing import Optional
 
 class Input(BaseModel):
-    first_name: str
-    last_name: str
-    age: int
-    degree: str
-    interest: Optional[str] = None
-
-    @validator('name')
-    def name_must_contain_space(cls, v):
-        if ' ' not in v:
-            raise ValueError('must contain a space')
-        return v.title()
-
-    @validator('password2')
-    def passwords_match(cls, v, values, **kwargs):
-        if 'password1' in values and v != values['password1']:
-            raise ValueError('passwords do not match')
-        return v
-
-    @validator('username')
-    def username_alphanumeric(cls, v):
-        assert v.isalnum(), 'must be alphanumeric'
-        return v
+    age: float
+    sex: float
+    bmi: float
+    bp: float
+    s1: float
+    s2: float
+    s3: float
+    s4: float
+    s5: float
+    s6: float
 
 class Output(BaseModel):
-    first_name: str
-    last_name: str
-    probability: float
-    acceptance: bool
+    progression: float
+    model: str
 
--- Serving/main.py
+++ Serving/main.py
@@ -1,28 +1,15 @@
 import random
-from fastapi import FastAPI, Query
+from fastapi import FastAPI
 from entities import Input, Output
-from utils import pre_process, model, post_process
+from processing import pre_process, post_process
+from models import linear_reg_model, lasso_model, dtree_model
 app = FastAPI()
 
-@app.post("/applications", response_model=Output)
-async def create_application(id: int, application: Input):
-
-    processed_inputs = await pre_process(Input)
+@app.post("/model", response_model=Output)
+async def serve(input: Input):
+    model = random.choice((linear_reg_model,lasso_model,dtree_model))
+    processed_inputs = await pre_process(input)
     predictions = await model(processed_inputs)
-    output = post_process(predictions, processed_inputs)
+    output = post_process(predictions, model.__name__)
     
-    return output
-    
-    
-    first_name = application.first_name
-    last_name = application.last_name
-    proba = random.random()
-    acceptance = proba > 0.5
-
-    output = {
-        "first_name": first_name,
-        "last_name": last_name,
-        "probability": proba,
-        "acceptance": acceptance,
-    }
     return output
