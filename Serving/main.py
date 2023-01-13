356825e2ee9688b957fb6be9e9b48c5b064700a6
karthik1900 <40236661+karthik1900@users.noreply.github.com>
2023-01-13 21:41:43 +0530

    Update main.py

--- Serving/main.py
+++ Serving/main.py
@@ -1,15 +1,16 @@
 import random
 from fastapi import FastAPI
-from entities import Input, Output
-from processing import pre_process, post_process
-from models import linear_reg_model, lasso_model, dtree_model
+from app.entities import Input, Output
+from app.processing import pre_process, post_process
+from app.models import linear_reg_model, lasso_model, dtree_model
 app = FastAPI()
 
 @app.post("/model", response_model=Output)
 async def serve(input: Input):
+    input = input.dict()
     model = random.choice((linear_reg_model,lasso_model,dtree_model))
     processed_inputs = await pre_process(input)
     predictions = await model(processed_inputs)
-    output = post_process(predictions, model.__name__)
+    output = post_process(predictions, model._name_)
     
     return output
